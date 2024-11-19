78e29425ddb9b3e25a5c76188afa831fac39c1b0
Danieü Oliveira <silvaoliveiradanieu@hotmail.com>
2024-11-19 16:35:55 -0300

    Remoção de linhas comentadas.

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,45 +1,4 @@
-﻿//let gravadorAudio;
-//let audioPartes = [];
-
-//$('#startButton').on('click', async function () {
-//    const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
-//    gravadorAudio = new MediaRecorder(stream);
-
-//    gravadorAudio.ondataavailable = event => audioChunks.push(event.data);
-//    gravadorAudio.onstop = async () => {
-//        const audioBlob = new Blob(audioChunks, { type: 'audio/wav' });
-//        const formData = new FormData();
-//        formData.append('audioFile', audioBlob, 'recordedAudio.wav');
-
-//        return $.ajax({
-//            url: '/api/audio/upload',
-//            type: 'POST',
-//            data: formData,
-//            processData: false,
-//            contentType: false,
-//            success: function () {
-//                alert('Áudio enviado com sucesso.');
-//            },
-//            error: function (err) {
-//                alert('Erro ao enviar áudio:', err);
-//            },
-//        });
-//    };
-
-//    gravadorAudio.start();
-//    $('#startButton').prop('disabled', true);
-//    $('#stopButton').prop('disabled', false);
-//});
-
-//$('#stopButton').on('click', function () {
-//    gravadorAudio.stop();
-//    $('#startButton').prop('disabled', false);
-//    $('#stopButton').prop('disabled', true);
-//    audioChunks = [];
-//});
-
-
-let gravadorAudio;
+﻿let gravadorAudio;
 let mp3Encoder;
 let audioPartes = [];
 let mp3Data = [];
@@ -47,6 +6,7 @@ let audioContexto;
 let processor;
 
 $('#startButton').on('click', async function () {
+    alert("Gravação iniciada");
 
     //peço permissão ao usuário para acessar o microfone, caso o usuário aceite, ela retorna um stream de áudio
     const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
